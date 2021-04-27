85dee278c28ede3d848455b130ff354c9d9518f9
Toni Ginard <toni.ginard@gmail.com>
2021-04-27 12:07:36 +0200

    Version 1.5

---
+++ README.md
@@ -0,0 +1,28 @@
+=== Plugin Name ===
+Contributors: toniginard, all jetpack contributors
+Tags: widget visibility
+Requires at least: 5.1
+Tested up to: 5.7.1
+Stable tag: 1.5
+Version: 1.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+
+
+== Description ==
+
+This plugin controls what pages your widgets appear on. It is based on module widget-visibility
+included in jetpack (https://wordpress.org/plugins/jetpack/)
+
+
+== Installation ==
+
+1. Upload 'widget-visibility-without-jetpack.php' to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Previous developers == 
+
+Eduardo Larequi
+Joan Boluda
+
--- widget-visibility-without-jetpack.php
+++ widget-visibility-without-jetpack.php
@@ -2,11 +2,10 @@
 
 /*
  * Plugin Name: Widget Visibility Without Jetpack
- * Plugin URI: http://boluda.com
- * Description: Control what pages your widgets appear on. Based on Widget Visibility module, from Jetpack plugin, http://wordpress.org/plugins/jetpack/. Created by Eduardo Larequi, mantained by Joan Boluda
- * Author: Joan Boluda
- * Version: 1.2
- * Author URI: http://boluda.com
+ * Plugin URI: https://github.com/toniginard/wordpress-widget-visibility-without-jetpack/
+ * Description: Control what pages your widgets appear on. Based on Widget Visibility module, from Jetpack plugin, https://wordpress.org/plugins/jetpack/
+ * Author: Toni Ginard
+ * Version: 1.5
  * License: GPL2+
  * Text Domain: jetpack
  * Domain Path: /languages/
